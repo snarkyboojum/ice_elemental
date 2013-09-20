2e3031bea469e0c1323c92e6897f12ce54ef8d01
Adrian White <snarkyboojum@gmail.com>
2013-09-20 13:59:34 +1000

    Added filewatcher

---
+++ Gemfile
@@ -0,0 +1,5 @@
+source "https://rubygems.org"
+
+gem 'aws-sdk'
+gem 'trollop'
+gem "listen", "~> 2.0.0.beta.2"
--- slam.rb
+++ slam.rb
@@ -1,13 +1,13 @@
 #!/usr/bin/env ruby
 
-require 'trollop'
-require 'AWS'
+require 'Bundler'
+require 'pathname'
+Bundler.require(:default)
 
 opts = Trollop::options do
-  opt :bucket, "Select the S3 bucket to export to",
-      :short => 'b', :type => String
-  opt :src, "Source directory to watch for archive import",
-      :short => 's', :type => String, :default => '.'
+  opt :bucket, "Select the S3 bucket to archive to",       :short => 'b', :type => String
+  opt :source, "Source directory to watch for archival",   :short => 's', :type => String, :default => '.'
+  opt :target, "Target directory to write S3 archives to", :short => 't', :type => String, :default => 'backups'
 end
 
 Trollop::die :bucket, "must be given" if not opts[:bucket_given]
@@ -17,18 +17,35 @@ s3 = AWS::S3.new(
   :secret_access_key => 'NXBdpcZTbdW7SXNx6q6Z4E2LfOOAILIRJgbx5Ohp'
 )
 
-bucket = s3.buckets[opts[:bucket]]
-
 # add bucket lifecycle policy to transition to glacier "immediately"
+bucket = s3.buckets[opts[:bucket]]
 bucket.lifecycle_configuration.update do
   #add_rule('backups/', :glacier_transition_time => 0)
 end
 
-# test writing an object to the backups/ folder
-obj = bucket.objects["backups/test2.txt"].write(:file => "/Users/whiteadr/Development/ice_elemental/test.txt")
-# or multipart upload if file is large, i.e. > 100MB
-#obj.write(file, :multipart_threshold => 100 * 1024 * 1024)
-
-bucket.objects.each do |obj|
-  puts obj.key
+# setup filesystem listener
+listener = Listen.to(opts[:source]) do |modified, added, removed|
+  if added
+    added.each do |path|
+      srcpath = Pathname.new(path)
+      putkey  = File.join(opts[:target], srcpath.basename)
+      puts srcpath.realpath
+      puts putkey
+      obj = bucket.objects[putkey].write(:file => srcpath.realpath)
+    end
+  end
+  puts "modified absolute path: #{modified}"
+  puts "added absolute path: #{added}"
+  puts "removed absolute path: #{removed}"
+  # or multipart upload if file is large, i.e. > 100MB
+  # obj.write(file, :multipart_threshold => 100 * 1024 * 1024)
+  # obj = bucket.objects["backups/test2.txt"].write(:file => "/Users/whiteadr/Development/ice_elemental/test.txt")
 end
+listener.start
+sleep
+
+
+#bucket.objects.each do |obj|
+#  puts obj.key
+#end
+
